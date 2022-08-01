50e30f057ad6faee5f002d42f0b6d9a855df6c4f
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-08-01 23:28:41 +1000

    Changed the port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const pageRouter = require('./routes/page');
 
 
 // Initialize express
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Middleware for parsing JSON
